fe0266b739dd78351f6b9c0e2b2922657d912527
Javion <jayred1127@gmail.com>
2020-09-01 00:16:31 -0400

    Added Fields for adding events and submit button

--- calender/src/App.js
+++ calender/src/App.js
@@ -7,11 +7,34 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <main>
-          <Calendar />
-        </main>
-      </div>
+		<div className="App">
+			<main>
+				<Calendar />
+			</main>
+		
+		<form>
+			<h1> 
+			Add Event to Calendar
+			</h1>
+			<label>
+			Event Name:<input type="text" name="event" id="event" />
+			</label>
+			<br />
+			<label>
+			Event Description:<input type="text" name="description" id="description" />
+			</label>
+			<br />
+			<label>
+			Start Day:<input type="datetime-local" name="startDate" id="startDate" />
+			</label>
+			<br />
+			<label>
+			End Day:<input type="datetime-local" name="endDate" id="endDate" />
+			</label>
+			<br />
+			<input type="submit" value="Add event to Calendar" id="submit" />
+		</form>
+		</div>
     );
   }
 }
